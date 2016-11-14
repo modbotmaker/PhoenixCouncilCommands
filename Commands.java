fb17f619409734686b390d4c869e2984b1c39279
Kyle Smith <smith.kyle1734@gmail.com>
2016-11-13 21:58:39 -0600

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -3,19 +3,24 @@ package me.modbot;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PhoenixCouncilCommands extends JavaPlugin{
+public class PhoenixCouncilCommands extends JavaPlugin {
 
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		logger logger = getLogger();
 		logger.info(pfdFile.getName() + " has been enabled!");
 	}
 	
-	public void onDisable(){
+	@Override
+	public void onDisable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		logger logger = getLogger();
 		logger.info(pfdFile.getName() + " has been disabled!");
+	}
+	
+	@Override
+	public void onCommand(CommandSender arg0, Command arg1, String arg2, String arg3) {
 		
 	}
 }
