aecf661a52a10f07bcc859a2fa8a74370c89bffc
modbotmaker <modbot3000@gmail.com>
2016-11-13 21:06:57 -0800

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -20,12 +20,19 @@ public class PhoenixCouncilCommands extends JavaPlugin {
 	}
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] a) {
-		if(!(sender instanceof Player)) {
-			sender.sendMessage("This command can be only run by a player!");
-			return false;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following...
+			// do something...
+			return true;
+		} else if (cmd.getName().equalsIgnoreCase("basic2")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+			} else {
+				Player player = (Player) sender;
+				// do something
+			}
+			return true;
 		}
 		return false;
-		
 	}
 }
