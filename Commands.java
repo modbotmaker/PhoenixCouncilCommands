a0f63bc2cef0d02d91fb4ffee32aba1b7afe2614
Kyle Smith <smith.kyle1734@gmail.com>
2016-11-14 19:04:46 -0600

    Mika pls...

--- Commands.java
+++ Commands.java
@@ -4,56 +4,35 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PhoenixCouncilCommands extends JavaPlugin {
-
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("mspc")) {
-			return true;
-		} else if (cmd.getName().equalsIgnoreCase("mspc")) {
-			if ((sender instanceof Player)) {
-				sender.sendMessage("Here is the MuseScore version of Phoenix Council here: https://musescore.com/groups/phoenixcouncil");
-			}
-			return true;
-		}
-		return false;
+	public boolean onEnable() {
+		// Enable stuff
+	}
+	
+	@Override
+	public boolean onDisable() {
+		// Disable stuff
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("steampc")) {
-			return true;
-		} else if (cmd.getName().equalsIgnoreCase("steampc")) {
-			if ((sender instanceof Player)) {
-				sender.sendMessage("Here is the Steam version of Phoenix Council here: http://steamcommunity.com/groups/Phoenixcouncil2");
+		if(sender instanceof Player) {
+			switch(cmd.getName()) {
+				case "mspc":
+					sender.sendMessage("Here is the MuseScore version of Phoenix Council here: https://musescore.com/groups/phoenixcouncil");
+					return true;
+				case "steampc":
+					sender.sendMessage("Here is the Steam version of Phoenix Council here: http://steamcommunity.com/groups/Phoenixcouncil2"); 
+					return true;
+				case "musescore":
+					sender.sendMessage("Here is Mika's MuseScore profile here: https://musescore.com/maestermika");
+					return true;
+				case "myt":
+					sender.sendMessage("Here is Mika's youtube channel: https://www.youtube.com/channel/UCiCT2ebIPRnEVHEcwpAuzhA");
+					return true;
+				default:
+					return false;
 			}
-			return true;
 		}
-		return false;
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("musescore")) {
-			return true;
-		} else if (cmd.getName().equalsIgnoreCase("musescore")) {
-			if ((sender instanceof Player)) {
-				sender.sendMessage("Here is Mika's MuseScore profile here: https://musescore.com/maestermika");
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("myt")) {
-			return true;
-		} else if (cmd.getName().equalsIgnoreCase("myt")) {
-			if ((sender instanceof Player)) {
-				sender.sendMessage("Here is Mika's youtube channel: https://www.youtube.com/channel/UCiCT2ebIPRnEVHEcwpAuzhA");
-			}
-			return true;
-		}
-		return false;
 	}
 }
